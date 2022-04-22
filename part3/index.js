cad715d28892bc37021a8d43a1090fcafaec5e60
Ming <mingx92@hotmail.com>
2022-04-23 02:28:48 +0800

    Exercise 3.8

--- part3/index.js
+++ part3/index.js
@@ -3,7 +3,8 @@ const app = express()
 var morgan = require('morgan')
 
 app.use(express.json())
-app.use(morgan('tiny'))
+morgan.token('post_body', function (req, res) {return JSON.stringify(req.body)})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post_body'))
 
 let contacts = [
     { 

9f677552bf81a762c800692582ec635d434e3c92
Ming <mingx92@hotmail.com>
2022-07-31 15:42:11 +0800

    Exercise 5.2

--- part4/bloglist-frontend/src/App.js
+++ part4/bloglist-frontend/src/App.js
@@ -18,6 +18,16 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+
   const handleLogin = async (event) => {
     event.preventDefault()
     
@@ -25,6 +35,9 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      ) 
       blogService.setToken(user.token)
       setUser(user)
       setUsername('')
@@ -37,6 +50,11 @@ const App = () => {
     }
   }
 
+  const handleLogout = async (event) => {
+    event.preventDefault()
+    setUser(null)
+    window.localStorage.removeItem('loggedNoteappUser')
+  }
   const loginForm = () => (
     <div>
       <Notification message={errorMessage} />
@@ -68,7 +86,7 @@ const App = () => {
   const blogForm = () => (
     <div>
      <h2>blogs</h2>
-     <p>{user.name} logged-in</p>
+     <p>User "<u>{user.name}</u>" has logged-in. <button onClick = {handleLogout}>logout</button></p> 
      {blogs.map(blog =>
      <Blog key={blog.id} blog={blog} />
        )}
